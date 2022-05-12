f6b1fba7a14ad64d0cf0bf7b76c772ed7fb8a073
Nilesh Patel <nilesh7patel86@gmail.com>
2022-05-12 10:54:26 +0530

    Initial Commit

---
+++ ratings-data-service/src/main/java/com/bvr/resources/RatingsResource.java
@@ -0,0 +1,30 @@
+package com.bvr.resources;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bvr.models.Rating;
+import com.bvr.models.UserRating;
+
+
+
+@RestController
+@RequestMapping("/ratingsdata")
+public class RatingsResource {
+
+    @RequestMapping("/movies/{movieId}")
+    public Rating getMovieRating(@PathVariable("movieId") String movieId) {
+        return new Rating(movieId, 4);
+    }
+
+    @RequestMapping("/user/{userId}")
+    public UserRating getUserRatings(@PathVariable("userId") String userId) {
+        UserRating userRating = new UserRating();
+        userRating.initData(userId);
+        return userRating;
+
+    }
+
+}
+
